247c1371532d338ef56ac13eeda142e5843aa459
Scott Raigner <scott.raigner@smarsh.com>
2017-05-31 12:32:37 -0700

    BEAC-13356 Fix JIRA/GitHub Integration

--- githubUtils/GetRepoList.py
+++ githubUtils/GetRepoList.py
@@ -7,16 +7,10 @@ import argparse
 parser = argparse.ArgumentParser(description='Get List of Repos')
 parser.add_argument('userName',help='your github username')
 parser.add_argument('accessToken',help='QAuth access token')
-#parser.add_argument('orgName',help='github Organization name')
-#parser.add_argument('repoName',help='The Target Repo Name - Example: ScottNewRepo')
-#parser.add_argument('branchName',help='The Branch to Protect')
 args = parser.parse_args()
 
 userName=args.userName
 accessToken=args.accessToken
-#orgName=args.orgName
-#repoName = args.repoName
-#branchName = args.branchName
 
 # this will need to be cleaned up a little ( use try etc. )
 g = Github(userName, accessToken)
